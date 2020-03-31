48095f59a8cfcc375e60ef1e15727efe0fe91b7d
xabier hernandez <exeibier@MacBook-Pro-de-xabier.local>
2020-03-30 21:55:03 -0600

    [add sticky menu]

--- front-1/js/main.js
+++ front-1/js/main.js
@@ -10,3 +10,16 @@ window.addEventListener('load', function() {
       }
     });
   });
+  // Sticy menu
+  $(document).ready(function() {
+    var altura = $('.menu').offset().top;
+
+    $(window).on('scroll', function() {
+
+        if ($(window).scrollTop() > altura) {
+            $('.menu').addClass('menu-fixed')
+        } else {
+            $('.menu').removeClass('menu-fixed')
+        }
+    })
+})
