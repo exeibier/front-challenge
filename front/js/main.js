4cc8920f315054a8078f93e8954ac37e0918537b
Mar0903 <cristiangm96@hotmail.com>
2020-03-30 21:14:29 -0600

    filter searching

--- front/js/main.js
+++ front/js/main.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
     })
 })
 
-
+//funionalidad para crear y mostrar post//
 fetchPosts()
 function fetchPosts() {
     $.ajax({
@@ -25,12 +25,13 @@ function fetchPosts() {
     })
 }
 
-
+var postArray = [] //herramienta que pronto vamos a ocupar
 function printPost(object) {
     $("#recent-post").empty()
     let hash
     for (hash in object) {
         let post = object[hash]
+        postArray.push(post)// nos va a servir para hacer la filtracion
         let postTopic = post.topic
         let postTitle = post.title
         let postDescription = post.description
@@ -90,3 +91,68 @@ function addButtonPostListener() {
     })
 }
 addButtonPostListener()
+//funionalidad para crear y mostrar post end//
+//filtracion de post
+
+function filterPosts() {
+    let criterialSearch = $("#search").val().toLowerCase()//obtener lo que tengo en mi buscardor
+    let filteredPosts = postArray.filter((post) => {
+        let topic = post.topic.toLowerCase()
+        let title = post.title.toLowerCase()
+        let description = post.description.toLowerCase()
+        if (topic.includes(criterialSearch)) {
+            return true
+        } else if (title.includes(criterialSearch)) {
+            return true
+        } else if (description.includes(criterialSearch)) {
+            return true
+        } else {
+            return false
+        }
+    })
+
+    $("#filtered-post-wrapper").empty()
+    if (filteredPosts.length === 0) {
+        let theresNothing = `<div class=row>
+        <div class="col-12 d-flex align-items-center text-center">
+        <h1>No se encontro lo que buscabas</h1>
+        </div>
+     </div>
+        `
+        $("#filtered-post-wrapper").html(theresNothing)
+    }
+
+    filteredPosts.forEach((post) => {
+        let postTopic = post.topic
+        let postTitle = post.title
+        let postDescription = post.description
+        let postAuthor = post.author
+        let postDate = post.date
+        let postImgUrl = post.url_image
+        let currentContent = $("#filtered-post-wrapper").html()
+        let newContent = `  <div class="row">
+        <div class="col-9">
+            <small class="text-uppercase">${postTopic}</small>
+            <h3>${postTitle}</h3>
+            <small class="extract">${postDescription}</small>
+            <p>${postAuthor}</p>
+            <small>${postDate}</small>
+        </div>
+        <div class="col-3 d-flex align-items-center"><img
+                src="${postImgUrl}"
+                alt=""></div>
+
+    </div>
+        `
+        $("#filtered-post-wrapper").html(currentContent + newContent)
+
+    })
+}
+function addSearchListener() {
+    $("#search").keypress((event) => {
+        if (event.which === 13) {
+            filterPosts()
+        }
+    })
+}
+addSearchListener()
