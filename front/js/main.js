5d9b44064f37adfce50bb9b19e7cd679d51efdd3
Mar0903 <cristiangm96@hotmail.com>
2020-03-30 18:08:14 -0600

    add post-functionality

--- front/js/main.js
+++ front/js/main.js
@@ -11,13 +11,82 @@ $(document).ready(function () {
     })
 })
 
+
+fetchPosts()
 function fetchPosts() {
     $.ajax({
         url: "https://javascript-ajax-d0ce6.firebaseio.com/team-xmm/posts/.json",
         method: "GET",
         success: (response) => {
             console.log('RES from GET', response)
+            printPost(response)
 
         }
     })
 }
+
+
+function printPost(object) {
+    $("#recent-post").empty()
+    let hash
+    for (hash in object) {
+        let post = object[hash]
+        let postTopic = post.topic
+        let postTitle = post.title
+        let postDescription = post.description
+        let postAuthor = post.author
+        let postDate = post.date
+        let postImgUrl = post.url_image
+        let currentContent = $("#recent-post").html()
+        let newContent = `  <div class="row">
+        <div class="col-9">
+            <small class="text-uppercase">${postTopic}</small>
+            <h3>${postTitle}</h3>
+            <small class="extract">${postDescription}</small>
+            <p>${postAuthor}</p>
+            <small>${postDate}</small>
+        </div>
+        <div class="col-3 d-flex align-items-center"><img
+                src="${postImgUrl}"
+                alt=""></div>
+
+    </div>
+        `
+        $("#recent-post").html(currentContent + newContent)
+    }
+}
+function getPostInfo() {
+    let postTopic = $("#topic").val()
+    let postTitle = $("#title").val()
+    let postDescription = $("#text-area").val()
+    let postAuthor = $("#author").val()
+    let postDate = $("#date").val()
+    let postImgUrl = $("#select-image").val()
+    let popularPost = $("#popular-box").val()
+    console.log(postTopic, postTitle, postDescription, postAuthor, postDate, postImgUrl, popularPost)
+    return {
+        topic: postTopic,
+        title: postTitle,
+        description: postDescription,
+        author: postAuthor,
+        date: postDate,
+        url_image: postImgUrl
+    }
+}
+function addButtonPostListener() {
+    let postButton = $(".btn-post")
+    postButton.click((event) => {
+        let postInfo = getPostInfo()
+        $.ajax({
+            url: "https://javascript-ajax-d0ce6.firebaseio.com/team-xmm/posts/.json",
+            method: "POST",
+            data: JSON.stringify(postInfo),
+            success: (response) => {
+                console.log('RES from POST:', response)
+                fetchPosts()
+            }
+        })
+
+    })
+}
+addButtonPostListener()
