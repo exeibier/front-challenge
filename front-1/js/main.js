f90f16d9a66aff9977f503f4fdb0e79f90af8bf7
xabier hernandez <exeibier@MacBook-Pro-de-xabier.local>
2020-03-30 21:40:14 -0600

    [mod modal and sticky]

--- front-1/js/main.js
+++ front-1/js/main.js
@@ -1,12 +1,12 @@
-$(document).ready(function() {
-    var altura = $('.menu').offset().top;
-
-    $(window).on('scroll', function() {
-
-        if ($(window).scrollTop() > altura) {
-            $('.menu').addClass('menu-fixed')
-        } else {
-            $('.menu').removeClass('menu-fixed')
-        }
-    })
-})
+window.addEventListener('load', function() {
+    let bar = document.getElementById('bar');
+    window.addEventListener('scroll', function() {
+      let scrollTop = document.body.scrollTop;
+      if (scrollTop < 600) {
+        bar.className = 'd-none d-md-block col-4 ';
+      }
+      if (scrollTop > 650) {
+        bar.className = 'd-none d-md-block col-4 bar-fixed';
+      }
+    });
+  });
