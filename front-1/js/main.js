7846c1042cc8e791fe7198f98f4ccb9c6a9387e2
Mar0903 <cristiangm96@hotmail.com>
2020-03-31 15:22:48 -0600

    fixed breacked things

--- front-1/js/main.js
+++ front-1/js/main.js
@@ -1,220 +1,226 @@
-window.addEventListener('load', function () {
-  let bar = document.getElementById('bar');
-  window.addEventListener('scroll', function () {
-      let scrollTop = document.body.scrollTop;
-      if (scrollTop < 550) {
-          bar.className = 'd-none d-md-block col-4 ';
-      }
-      if (scrollTop > 550) {
-          bar.className = 'd-none d-md-block col-4 bar-fixed';
-      }
-  });
-});
 // Sticy menu
 $(document).ready(function () {
-  var altura = $('.menu').offset().top;
+    var altura = $('.menu').offset().top;
 
-  $(window).on('scroll', function () {
+    $(window).on('scroll', function () {
 
-      if ($(window).scrollTop() > altura) {
-          $('.menu').addClass('menu-fixed')
-      } else {
-          $('.menu').removeClass('menu-fixed')
-      }
-  })
+        if ($(window).scrollTop() > altura) {
+            $('.menu').addClass('menu-fixed')
+        } else {
+            $('.menu').removeClass('menu-fixed')
+        }
+    })
 })
+
+window.addEventListener('load', function () {
+    let bar = document.getElementById('bar');
+    window.addEventListener('scroll', function () {
+        let scrollTop = document.body.scrollTop;
+        if (scrollTop < 550) {
+            bar.className = 'd-none d-md-block col-4 ';
+        }
+        if (scrollTop > 550) {
+            bar.className = 'd-none d-md-block col-4 bar-fixed';
+        }
+    });
+});
 
 //funionalidad para crear y mostrar post//
 fetchPosts()
 function fetchPosts() {
-  $.ajax({
-      url: "https://javascript-ajax-d0ce6.firebaseio.com/team-xmm/posts/.json",
-      method: "GET",
-      success: (response) => {
-          printPost(response)
+    $.ajax({
+        url: "https://javascript-ajax-d0ce6.firebaseio.com/team-xmm/posts/.json",
+        method: "GET",
+        success: (response) => {
+            printPost(response)
 
-      }
-  })
+        }
+    })
 }
 
 var postArray = [] //herramienta que pronto vamos a ocupar
 function printPost(object) {
-  $("#recent-post").empty()
-  let hash
-  for (hash in object) {
-      let post = object[hash]
-      postArray.push(post)// nos va a servir para hacer la filtracion
-      let postTopic = post.topic
-      let postTitle = post.title
-      let postDescription = post.description
-      let postAuthor = post.author
-      let postDate = post.date
-      let postImgUrl = post.url_image
-      let currentContent = $("#recent-post").html()
-      let newContent = ` 
+    $("#recent-post").empty()
+    let hash
+    for (hash in object) {
+        let post = object[hash]
+        postArray.push(post)// nos va a servir para hacer la filtracion
+        let postTopic = post.topic
+        let postTitle = post.title
+        let postDescription = post.description
+        let postAuthor = post.author
+        let postDate = post.date
+        let postImgUrl = post.url_image
+        let currentContent = $("#recent-post").html()
+        let newContent = ` 
       <div class="row border-bottom m-top-2">
-                        <div class="col-8  ">
+        <div class="col-8  ">
+            <button type="button" class="btn text-left" data-toggle="modal" data-target="#content">
+                    <small class="text-uppercase">${postTopic}</small>
+                    <h2 class="font-weight-bolder pa-ma  col-8 align-self-left">${postTitle} </h2>
+                    <p class="text-secondary extract">${postDescription}</p>
+                    <p>${postAuthor}</p>
+                    <p class="text-secondary">${postDate}</p>
+            </button>
+        </div>
+        <div class="col-4 pop-img">
+            <img src=${postImgUrl} style="width: 80%;"  alt="...">
 
-                            <button type="button" class="btn text-left" data-toggle="modal" data-target="#content">
-                                
-                                    <p class="text-secondary">Popular on Medium</p>
-                                    <small class="text-uppercase">${postTopic}</small>
-                                    <h2 class="font-weight-bolder pa-ma  col-8 align-self-left">${postTitle} </h2>
-                                    <p class="text-secondary extract">${postDescription}</p>
-                                    <p>${postAuthor}</p>
-                                    <p class="text-secondary">${postDate}</p>
-                                
-                            </button>
-                        </div>
-                        <div class="col-4 pop-img">
-                            <img src=${postImgUrl} style="width: 80%;"  alt="...">
-
-                        </div>  
+        </div>  
       `
-      $("#recent-post").html(currentContent + newContent)
-  }
-  filterByPopularity()
+        $("#recent-post").html(currentContent + newContent)
+    }
+    filterByPopularity()
 }
 function getPostInfo() {
-  let postTopic = $("#topic").val()
-  let postTitle = $("#title").val()
-  let postDescription = $("#text-area").val()
-  let postAuthor = $("#author").val()
-  let postDate = $("#date").val()
-  let postImgUrl = $("#select-image").val()
-  let popularPost = $("#popular-box").is(":checked")
-  return {
-      topic: postTopic,
-      title: postTitle,
-      description: postDescription,
-      author: postAuthor,
-      date: postDate,
-      url_image: postImgUrl,
-      is_popular: popularPost
-  }
+    let postTopic = $("#topic").val()
+    let postTitle = $("#title").val()
+    let postDescription = $("#text-area").val()
+    let postAuthor = $("#author").val()
+    let postDate = $("#date").val()
+    let postImgUrl = $("#select-image").val()
+    let popularPost = $("#popular-box").is(":checked")
+    return {
+        topic: postTopic,
+        title: postTitle,
+        description: postDescription,
+        author: postAuthor,
+        date: postDate,
+        url_image: postImgUrl,
+        is_popular: popularPost
+    }
 }
 function addButtonPostListener() {
-  let postButton = $(".btn-post")
-  postButton.click((event) => {
-      let postInfo = getPostInfo()
-      $.ajax({
-          url: "https://javascript-ajax-d0ce6.firebaseio.com/team-xmm/posts/.json",
-          method: "POST",
-          data: JSON.stringify(postInfo),
-          success: (response) => {
-              fetchPosts()
-          }
-      })
+    let postButton = $(".btn-post")
+    postButton.click((event) => {
+        let postInfo = getPostInfo()
+        $.ajax({
+            url: "https://javascript-ajax-d0ce6.firebaseio.com/team-xmm/posts/.json",
+            method: "POST",
+            data: JSON.stringify(postInfo),
+            success: (response) => {
+                fetchPosts()
+            }
+        })
 
-  })
+    })
 }
 addButtonPostListener()
 //funionalidad para crear y mostrar post end//
 //filtracion de post
 
 function filterPosts() {
-  let criterialSearch = $("#search").val().toLowerCase()//obtener lo que tengo en mi buscardor
-  let filteredPosts = postArray.filter((post) => {
-      let topic = post.topic.toLowerCase()
-      let title = post.title.toLowerCase()
-      let description = post.description.toLowerCase()
-      if (topic.includes(criterialSearch)) {
-          return true
-      } else if (title.includes(criterialSearch)) {
-          return true
-      } else if (description.includes(criterialSearch)) {
-          return true
-      } else {
-          return false
-      }
-  })
+    let criterialSearch = $("#search").val().toLowerCase()//obtener lo que tengo en mi buscardor
+    let filteredPosts = postArray.filter((post) => {
+        let topic = post.topic.toLowerCase()
+        let title = post.title.toLowerCase()
+        let description = post.description.toLowerCase()
+        if (topic.includes(criterialSearch)) {
+            console.log('funciona 1')
+            return true
+        } else if (title.includes(criterialSearch)) {
+            console.log('funciona 2')
+            return true
+        } else if (description.includes(criterialSearch)) {
+            console.log('funciona 3')
+            return true
+        } else {
+            console.log('NO funciona')
+            return false
+        }
+    })
 
-  $("#filtered-post-wrapper").empty()
-  if (filteredPosts.length === 0) {
-      let theresNothing = `<div class=row>
+    $("#top").empty()
+    if (filteredPosts.length === 0) {
+        let theresNothing = `<div class=row>
       <div class="col-12 d-flex align-items-center text-center">
       <h1>No se encontro lo que buscabas</h1>
       </div>
    </div>
       `
-      $("#filtered-post-wrapper").html(theresNothing)
-  }
+        $("#top").html(theresNothing)
+    }
 
-  filteredPosts.forEach((post) => {
-      let postTopic = post.topic
-      let postTitle = post.title
-      let postDescription = post.description
-      let postAuthor = post.author
-      let postDate = post.date
-      let postImgUrl = post.url_image
-      let currentContent = $("#filtered-post-wrapper").html()
-      let newContent = `  <div class="row">
-      <div class="col-9">
-          <small class="text-uppercase">${postTopic}</small>
-          <h3>${postTitle}</h3>
-          <small class="extract">${postDescription}</small>
-          <p>${postAuthor}</p>
-          <small>${postDate}</small>
-      </div>
-      <div class="col-3 d-flex align-items-center"><img
-              src="${postImgUrl}"
-              alt=""></div>
+    filteredPosts.forEach((post) => {
+        console.log(post)
+        let postTopic = post.topic
+        let postTitle = post.title
+        let postDescription = post.description
+        let postAuthor = post.author
+        let postDate = post.date
+        let postImgUrl = post.url_image
+        let currentContent = $("#top").html()
+        let newContent = `
+        <div class="row">
+            <div class="col-9">
+                <small class="text-uppercase">${postTopic}</small>
+                <h3>${postTitle}</h3>
+                <small class="extract">${postDescription}</small>
+                <p>${postAuthor}</p>
+                <small>${postDate}</small>
+            </div>
+            <div class="col-3 d-flex align-items-center">
+                <img src="${postImgUrl}" alt="">
+            </div>
+        </div>
+        `
+        $("#top").html(currentContent + newContent)
 
-  </div>
-      `
-      $("#filtered-post-wrapper").html(currentContent + newContent)
-
-  })
+    })
 }
 function addSearchListener() {
-  $("#search").keypress((event) => {
-      event.preventDefault()
-      if (event.which === 13) {
-          filterPosts()
-      }
-  })
+    $("#search").keypress((event) => {
+        if (event.which === 13) {
+            event.preventDefault()
+            filterPosts()
+        }
+    })
 }
 addSearchListener()
 
 function filterByPopularity() {
-  let popularPost = postArray.filter((post) => {
-      return post.is_popular
-  })
-  let topFive = popularPost.slice(0, 5)
-  $("#popular-post").empty()
-  topFive.forEach((post, index) => {
-      let postTitle = post.title
-      let postAuthor = post.author
-      let postDate = post.date
-      let currentContent = $("#popular-post").html()
-      let newContent = `
-       <button type="button" class="btn text-left" data-toggle="modal" data-target="#content">
-          <article class="bg-white ">
-              <a href="">
-                  <h2 class="font-weight-bolder">${postTitle}</h2>
-              </a>
-              <p>${postAuthor}</p>
-              <p>${postDate}</p>
-          </article>          
-       </button>
-      `
-      $("#popular-post").html(currentContent + newContent)
+    let popularPost = postArray.filter((post) => {
+        return post.is_popular
+    })
+    let topFive = popularPost.slice(0, 5)
+    $("#popular-post").empty()
+    topFive.forEach((post, index) => {
+        let postTitle = post.title
+        let postAuthor = post.author
+        let postDate = post.date
+        let currentContent = $("#popular-post").html()
+        let newContent = `
+        <div class="row">
+        <div class="col-3">
+            <h1 class="text-muted">0${index + 1}</h1>
+        </div>
+        <div class="col-8">
+        <button type="button" class="btn text-left" data-toggle="modal" data-target="#content">
+            <article class="bg-white ">
+                <h2 class="font-weight-bolder">${postTitle}</h2>
+                <p>${postAuthor}</p>
+                <p>${postDate}</p>
+            </article>          
+        </button>
+        </div>
+        </div
+        `
+        $("#popular-post").html(currentContent + newContent)
 
-  })
+    })
 }
 
-
-function blogEntry () {
-  let hash
-  for (hash in object) {
-      let post = object[hash]
-      postArray.push(post)
-      let postTitle = post.title
-      let postDescription = post.description
-      let postAuthor = post.author
-      let postDate = post.date
-      let postImgUrl = post.url_image
-      let newContent = ` 
+function blogEntry() {
+    let hash
+    for (hash in object) {
+        let post = object[hash]
+        postArray.push(post)
+        let postTitle = post.title
+        let postDescription = post.description
+        let postAuthor = post.author
+        let postDate = post.date
+        let postImgUrl = post.url_image
+        let newContent = ` 
       <div class="modal fade" id="content" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
       <div class="modal-dialog" role="document">
         <div class="modal-content">
@@ -277,16 +283,16 @@ function blogEntry () {
       </div>
     </div>
       `
-      newContent = addModalListener()
+        newContent = addModalListener()
     }
     filterByPopularity()
-  }
+}
 
-  function addModalListener () {
-  $(document).ready(function(){
-    $("#recent-post").click(function(){
-      $("#content").modal();
+function addModalListener() {
+    $(document).ready(function () {
+        $("#recent-post").click(function () {
+            $("#content").modal();
+        });
     });
-  });
 }
 
